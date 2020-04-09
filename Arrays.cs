c54988cefc2fe25934b78365cbdf5f542494b42c
Remus <remusiftimie87@gmail.com>
2020-04-09 15:30:37 +0100

    Added strings and arrays solutions

--- Arrays.cs
+++ Arrays.cs
@@ -92,7 +92,7 @@ namespace Practice_April2020
             {
                 aryCopy[i] = ary[i];
             }
-
+            // Display
             Console.Write("The elements stored inthe first array are: ");
             for (i = 0; i < n; i++)
             {
@@ -199,6 +199,142 @@ namespace Practice_April2020
             }
             Console.Write("\n\n");
            
+        }
+
+        //7
+        public void Merge2Arrays_SortAscending()
+        {
+            int num1, num2, num3;
+            int i, j, k;
+            int[] ary1 = new int[100];
+            int[] ary2 = new int[100];
+            int[] aryMerged = new int[100];
+
+            Console.Write("Input number of elements from first array: \n");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            for (i = 0; i < num1; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                ary1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("Input number of elements from second array: \n");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            for (i = 0; i < num2; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                ary2[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            // size of the merged array
+            num3 = num1 + num2;
+
+            // insert in the aryMerged
+            for (i = 0; i < num1; i++)
+            {
+                aryMerged[i] = ary1[i];
+            }
+            for (j = 0; j < num2; j++)
+            {
+                aryMerged[i] = ary2[j];
+                i++;
+            }
+
+            // Sort the array in ascending order
+            for (i = 0; i < num3; i++)
+            {
+                for (k = 0; k < num3-1; k++)
+                {
+                    if (aryMerged[k] >= aryMerged[k+1])
+                    {
+                        // Swap
+                        j = aryMerged[k + 1];
+                        aryMerged[k + 1] = aryMerged[k];
+                        aryMerged[k] = j;
+                    }
+                }
+            }
+            // Display
+            Console.Write("\nThe merged array in ascending ordrer is: \n");
+            for (i = 0; i < num3; i++)
+            {
+                Console.WriteLine("{0} ", aryMerged[i]);
+            }
+        }
+
+        //8
+        public void CountElementFrequency()
+        {
+            int i, j;
+            int n, counter;
+            int[] multime = new int[100];
+            int[] frequency1 = new int[100];
+
+            Console.Write("Input the number of elements to be stored in the array: \n");
+            n = int.Parse(Console.ReadLine());
+            Console.Write("Input {0} elements in the array\n", n);
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("element {0} : ", i);
+                multime[i] = int.Parse(Console.ReadLine());
+                frequency1[i] = 1;
+            }
+
+            // Find the frequency for each element
+            for (i = 0; i < n; i++)
+            {
+                counter = 1;
+                for (j = i+1; j < n; j++)
+                {
+                    if (multime[i] == multime[j])
+                    {
+                        counter++;
+                        frequency1[j] = 0;
+                    }
+                }
+                if (frequency1[i] != 0)
+                {
+                    frequency1[i] = counter;
+                }
+            }
+            Console.Write("\nThe frequency of all elements of the array: \n");
+            for (i = 0; i < n; i++)
+            {
+                if (frequency1[i] != 0)
+                {
+                    Console.Write("{0} occurs {1} times\n", multime[i], frequency1[i]);
+                }
+            }
+        }
+
+        //9
+        public static void MaximumAndMinimumElement()
+        {
+            int n, max, min;
+            int[] arr = new int[100];
+            Console.Write("Input the number of elements to be stored: \n");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input {0} elements in the array\n", n);
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Element {0} : ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            max = arr[0];
+            min = arr[0];
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            Console.Write("Maximum element is {0}\n", max);
+            Console.Write("Minimum element is {0}\n", min);
         }
     }
 }
---
+++ Strings.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice_April2020
+{
+    class Strings
+    {
+        // 3
+        public void SeparateChars()
+        {
+            int l = 0;
+            Console.WriteLine("Input a string: ");
+            string str = Console.ReadLine();
+            while (l <= str.Length-1)
+            {
+                Console.Write("{0} ", str[l]);
+                l++;
+            }
+        }
+
+        // 5
+        public void CountTotalWords()
+        {
+            string str;
+            
+            int l = 0, wrd = 1;
+            Console.Write("Input a string\n");
+            str = Console.ReadLine();
+            Console.WriteLine("Total number of words is: ");
+            while (l <= str.Length-1)
+            {
+                // check char if is white space or new line or tab
+                if (str[l] == ' ' || str[l] == '\n' || str[l] == '\t')
+                {
+                    wrd++;
+                }
+                l++;
+            }
+            Console.WriteLine("The total number of words is: {0}", wrd);
+        }
+
+        // 7
+        public void CountChar_AlphabetsDigitsSpecials()
+        {
+            string str;
+            int nAlphabets = 0, nDigits = 0, nSpecials = 0;
+
+            Console.WriteLine("Input a string");
+            str = Console.ReadLine();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >='A' && str[i] <= 'Z'))
+                {
+                    nDigits++;
+                }
+                else if (str[i] >= '0' && str[i] <= '9')
+                {
+                    nAlphabets++;
+                }
+                else
+                {
+                    nSpecials++;
+                }
+            }
+            Console.WriteLine("Number of Alphabets in the string is: {0}", nAlphabets);
+            Console.WriteLine("Number of Digits in the string is: {0}", nDigits);
+            Console.WriteLine("Number of Special in the string is: {0}", nSpecials);
+
+        }
+
+        // 8
+        public void CopyStrings()
+        {
+            string str;
+            int i, l;
+            Console.Write("Input a string: \n");
+            str = Console.ReadLine();
+            l = str.Length;
+            string[] strCopy = new string[l];
+            // Copies str to trCopy character to character
+            i = 0;
+            while (i < l)
+            {
+                string temp = str[i].ToString();
+                strCopy[i] = temp;
+                i++;
+            }
+            Console.Write("\n The first string is {0}\n", str);
+            Console.Write("The second string is: {0}\n", string.Join("", strCopy));
+            Console.WriteLine("The number of characters copied is: {0}\n", i);
+        
+        }
+
+        // 10
+        public void MaxOccuring_Char()
+        {
+
+            string str;
+            int ascii, max, i = 0; ;
+            int[] ch_fre = new int[255];
+            Console.WriteLine("Input a string: ");
+            str = Console.ReadLine();
+            int l = str.Length;
+            // Set frequency of all chars to 0
+            for (i = 0; i < 255; i++)
+            {
+                ch_fre[i] = 0;
+            }
+            // Read frequency of each characters
+            i = 0;
+            while (i < l)
+            {
+                ascii = str[i];
+                ch_fre[ascii] += 1;
+                i++;
+            }
+
+            max = 0;
+            for (i = 0; i < 255; i++)
+            {
+                if (i != 32)
+                {
+                    if (ch_fre[i] > ch_fre[max])
+                        max = i;
+                }
+            }
+            Console.Write("The Highest frequency of character '{0}' is" +
+                " appearing for number of times : {1} \n", (char)max, ch_fre[max]);
+
+
+        }
+
+        // 11
+        public void SortStringArray()
+        {
+            string str;
+            int l;
+            char ch;
+            Console.Write("Input a string: \n");
+            str = Console.ReadLine();
+            l = str.Length;
+            char[] strArr = str.ToCharArray(0, l);
+            for (int i = 1; i < l; i++)
+                for (int j = 0; j < l-i; j++)
+                    if (strArr[j] > strArr[j + 1])
+                    {
+                        // SWAP *Good algo for sorting
+                        ch = strArr[j];
+                        strArr[j] = strArr[j + 1];
+                        strArr[j + 1] = ch;
+                    }
+            Console.Write("After sorting the string appears like: \n");
+            foreach (var c in strArr)
+            {
+                ch = c;
+                Console.Write("{0} ", ch);
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
